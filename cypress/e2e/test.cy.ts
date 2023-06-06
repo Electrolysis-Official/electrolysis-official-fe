1f3124122d6639a895d28c848ba80a9ad2ab9f55
karimy <karim.alrashdan14@gmail.com>
2023-06-06 17:30:50 -0600

    Test NavBar contents

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
+describe('Content of Home Page', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
   })
+  it('Should show the NavBar Contents', () => {
+    cy.get("nav").should("be.visible")
+    .should("contain", "Home")
+    .should("contain", "Electrolysis")
+    .should("contain", "Services")
+    .should("contain", "About Me")
+    .should("contain", "Client History and Consent Form")
+    .should("contain", "Before/After Care")
+  })
+  
 })
