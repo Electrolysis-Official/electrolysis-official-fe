2e7ebba35e532b38dd68a01a336d5be7a1b387c0
karimy <karim.alrashdan14@gmail.com>
2023-06-06 19:34:49 -0600

    Create Services component

---
+++ app/Services/page.tsx
@@ -0,0 +1,46 @@
+const Services = () => {
+  return (
+    <main>
+      <h1>Services</h1>
+
+<p className="services-paragraph1">Here at T4T Electrolysis, we treat everyone but specialize in the needs of the trans/queer community - which primarily means working on facial hair, genital hair, surgery prep, and custom work to help affirm your gender in a way that’s unique to you. <br />
+
+We treat between 15 minutes and 2 hour long sessions <br />
+
+Cash and check only - no venmo or cards please</p>
+
+<h2>Prices:</h2> 
+<ul>
+<li>15 minutes: $20</li>
+<li>30 minutes: $40</li>
+<li>45 minutes: $60</li>
+<li>1 hour: $80</li>
+
+<li>1 hour 15 minutes: $100</li>
+<li>1 hour 30 minutes: $120</li>
+<li>1 hour 45 minutes: $140 </li>
+<li>2 hours: $160</li>
+</ul>
+
+<h3>Other common areas that are treated include: </h3>
+<ul>
+<li>Upper lip </li>
+<li>Sideburns</li>
+<li>Brow shaping </li>
+<li>Cleaning up beard hairs around the edges </li>
+<li>Back hair </li>
+<li>Armpits</li>
+<li>Bikini line </li>
+<li>Buttocks </li>
+<li>Toe/finger knuckles </li>
+<li>Happy trails </li>
+<li>Nipples </li>
+<li>Thinning of areas to have less dense hair</li>
+<li>And more, just inquire!  </li>
+</ul>  
+
+    </main>
+  )
+}
+
+export default Services;
